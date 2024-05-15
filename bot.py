31d82489065f14357a1b0a901bd8006b1d22ca00
Cee2kay <crusherx99@gmail.com>
2024-05-15 05:20:47 +0300

    arabic memes addition

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import dotenv_values
 import commands as com
-
-from discord.ext import commands
+from discord.ext import commands, tasks
+from discord import app_commands
 import discord
-
+from util import meme_getter, meme_getter_ar
+config = dotenv_values(".env")
+TOKEN = config['TOKEN']
 
 class MyBot(discord.ext.commands.Bot):
     async def on_ready(self):
@@ -18,5 +21,41 @@ bot: discord.ext.commands.Bot = MyBot(command_prefix='.', intents=intents)
 async def memes(interaction: discord.Interaction, amount: int = 10):
     await com.multiple_memes(interaction, amount)
 
+@tasks.loop(hours=24)
+async def memesDailyloop():
 
-bot.run(os.environ.get("DISCORD_TOKEN"))
+  #print("is the function being called")
+  print("Daily Memes Loop has started") 
+  amount = 10
+  amount_ar = 3
+  guilds = bot.guilds
+
+
+  for guild in guilds:
+
+   for channel in guild.channels:
+
+    if channel.name == "test" or channel.name == "memes":
+
+      urls = meme_getter.meme_urls(amount)
+
+      #urls.pop(0)
+
+      for url in urls:
+        await channel.send(url)
+    elif  channel.name == "ساعه-لقلبك":
+      urls = meme_getter_ar.meme_urls(amount_ar)
+
+      #urls.pop(0)
+
+      for url in urls:
+        await channel.send(url)
+       
+
+      
+@bot.event
+async def on_ready():
+    print("bot has started")
+    memesDailyloop.start()
+    
+bot.run(TOKEN)
---
+++ util/meme_getter_ar.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+
+def meme_urls(amount: int):
+    response = requests.get("https://meme-api.com/gimme/Egy_Memes/" + str(amount))
+    memes = json.loads(response.content)["memes"]
+    urls = []
+    for meme in memes:
+        urls.append(meme["url"])
+    return urls
